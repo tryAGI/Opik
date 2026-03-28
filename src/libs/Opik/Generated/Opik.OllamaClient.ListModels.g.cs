654f35953683e0ca3a8b03ab0ea525b6616bce6c
Konstantin S. <havendv@gmail.com>
2026-03-29 01:29:15 +0400

    feat: Updated OpenAPI spec (#10)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Opik/Generated/Opik.OllamaClient.ListModels.g.cs
+++ src/libs/Opik/Generated/Opik.OllamaClient.ListModels.g.cs
@@ -226,7 +226,7 @@ namespace Opik
                     __response.EnsureSuccessStatusCode();
 
                     return
-                        global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::Opik.OllamaModel>), JsonSerializerContext) as global::System.Collections.Generic.IList<global::Opik.OllamaModel> ??
+                        (global::System.Collections.Generic.IList<global::Opik.OllamaModel>?)global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::Opik.OllamaModel>), JsonSerializerContext) ??
                         throw new global::System.InvalidOperationException($"Response deserialization failed for \"{__content}\" ");
                 }
                 catch (global::System.Exception __ex)
@@ -257,7 +257,7 @@ namespace Opik
                     ).ConfigureAwait(false);
 
                     return
-                        await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::Opik.OllamaModel>), JsonSerializerContext).ConfigureAwait(false) as global::System.Collections.Generic.IList<global::Opik.OllamaModel> ??
+                        (global::System.Collections.Generic.IList<global::Opik.OllamaModel>?)await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::Opik.OllamaModel>), JsonSerializerContext).ConfigureAwait(false) ??
                         throw new global::System.InvalidOperationException("Response deserialization failed.");
                 }
                 catch (global::System.Exception __ex)
