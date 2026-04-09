d2e1bb9708e97cd71d29c41f7adf0d0d2090716f
Konstantin S. <havendv@gmail.com>
2026-04-09 20:17:37 +0400

    feat: Updated OpenAPI spec (#29)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Opik/Generated/Opik.IExperimentsClient.ExecuteExperiment.g.cs
@@ -0,0 +1,41 @@
+#nullable enable
+
+namespace Opik
+{
+    public partial interface IExperimentsClient
+    {
+        /// <summary>
+        /// Create and execute experiment<br/>
+        /// Creates experiments for each prompt variant and asynchronously processes all dataset items
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::Opik.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::Opik.ExperimentExecutionResponse> ExecuteExperimentAsync(
+
+            global::Opik.ExperimentExecutionRequest request,
+            global::System.Threading.CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Create and execute experiment<br/>
+        /// Creates experiments for each prompt variant and asynchronously processes all dataset items
+        /// </summary>
+        /// <param name="datasetName"></param>
+        /// <param name="datasetVersionId"></param>
+        /// <param name="prompts"></param>
+        /// <param name="projectName"></param>
+        /// <param name="datasetId"></param>
+        /// <param name="versionHash"></param>
+        /// <param name="promptVersions"></param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::System.InvalidOperationException"></exception>
+        global::System.Threading.Tasks.Task<global::Opik.ExperimentExecutionResponse> ExecuteExperimentAsync(
+            string datasetName,
+            global::System.Collections.Generic.IList<global::Opik.PromptVariant> prompts,
+            global::System.Guid datasetId,
+            global::System.Guid? datasetVersionId = default,
+            string? projectName = default,
+            string? versionHash = default,
+            global::System.Collections.Generic.IList<global::Opik.PromptVersionLink>? promptVersions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
+    }
+}
--- src/libs/Opik/Generated/Opik.Models.Message.g.cs
+++ src/libs/Opik/Generated/Opik.Models.Message.g.cs
@@ -8,11 +8,47 @@ namespace Opik
     /// </summary>
     public sealed partial class Message
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("role")]
+        [global::System.Text.Json.Serialization.JsonRequired]
+        public required string Role { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("content")]
+        [global::System.Text.Json.Serialization.JsonRequired]
+        public required global::Opik.JsonNode Content { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
         public global::System.Collections.Generic.IDictionary<string, object> AdditionalProperties { get; set; } = new global::System.Collections.Generic.Dictionary<string, object>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Message" /> class.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="content"></param>
+#if NET7_0_OR_GREATER
+        [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
+#endif
+        public Message(
+            string role,
+            global::Opik.JsonNode content)
+        {
+            this.Role = role ?? throw new global::System.ArgumentNullException(nameof(role));
+            this.Content = content ?? throw new global::System.ArgumentNullException(nameof(content));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Message" /> class.
+        /// </summary>
+        public Message()
+        {
+        }
     }
 }
